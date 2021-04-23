bbf30a61f44ba9917780f2563af9f281152bcda3
Revan <revan@ualberta.ca>
2021-04-22 20:26:43 -0600

    ready for ablation 2

--- experiments_ablation.py
+++ experiments_ablation.py
@@ -19,7 +19,7 @@ def get_args():
 
     parser.add_argument('--output_type',  default= "compute_canada_format", type=str, choices=("bash_file", "compute_canada_format", "execute"), help="What should be the output of this file: bash_file: generates a bash file of the commands that you should run in linux for all the experiments | compute_canada_format: generates a file that can be used to run all the experiments on compute canada | execute: will run all experiments on your computer")
 
-    parser.add_argument('--output_path', default='experiments', type=str,
+    parser.add_argument('--output_path', default='experiments_ablation', type=str,
             nargs='?', help="The path to save the output file of this script")
 
     parser.add_argument('--save_path', default='results', type=str,
@@ -46,7 +46,7 @@ GENERAL_ARGS = {
 
 
 # default Arguments for RNN
-RNN_ARGS = {
+ZERO_RNN_ARGS = {
     'model_arch' : 'RNN',
     'buffer_size' : 10000,
     'batch_size' : 64,
@@ -66,8 +66,28 @@ RNN_ARGS = {
     'zero_state' : True
     }
 
-# default Arguments for FFN
-FFN_ARGS = {
+# default Arguments for RNN
+RNN_ARGS = {
+    'model_arch' : 'RNN',
+    'buffer_size' : 10000,
+    'batch_size' : 64,
+    'hidden_layer_size' : 64,
+    'num_layers' : 0,
+    'seq_len' : 1,
+    'tau' : 1,
+    'learning_starts': 100,
+    'learning_freq' : 100,
+    'target_update_freq' : 1,
+    'learning_rate' : 5e-4,
+    'epsilon': 1, # all methods use epsilon greedy 
+    'min_epsilon' : 0.01,
+    'decay' : 0.995,
+    'state_representation' : 'one_hot',
+    'buffer' : 'episodes'
+    }
+
+# default Arguments for FFN with modified buffer
+FFN_MB_ARGS = {
     'model_arch' : 'FFN',
     'buffer_size' : 10000,
     'batch_size' : 64,
@@ -83,6 +103,25 @@ FFN_ARGS = {
     'decay' : 0.995,
     'state_representation' : 'one_hot',
     'buffer' : 'episodes',
+    'seq_len' : 1
+    }
+
+FFN_ARGS = {
+    'model_arch' : 'FFN',
+    'buffer_size' : 10000,
+    'batch_size' : 64,
+    'hidden_layer_size' : 64,
+    'num_layers' : 2,
+    'tau' : 1,
+    'learning_starts': 100,
+    'learning_freq' : 100,
+    'target_update_freq' : 1,
+    'learning_rate' : 5e-4,
+    'epsilon': 1, # all methods use epsilon greedy
+    'min_epsilon' : 0.01,
+    'decay' : 0.995,
+    'state_representation' : 'one_hot',
+    'buffer' : 'steps',
     'seq_len' : 1
     }
 
@@ -134,11 +173,11 @@ SEEDS = np.random.randint(0, 10000, size=50)
 
 # determined though new_visualizations script
 BEST_PARAMS = [
-    ('FFN', '1', 'envs:random_maze-v0', '1', '10', '5e-05', '10000'),
-    ('RNN', '1', 'envs:random_maze-v0', '1', '1', '0.0005', '10000'),
-    ('RNN', '2', 'envs:random_maze-v0', '1', '100', '0.0005', '10000'),
-    ('RNN', '4', 'envs:random_maze-v0', '1', '100', '5e-05', '10000'),
-    ('RNN', '8', 'envs:random_maze-v0', '1', '1', '5e-05', '10000'),
+    # ('FFN', '1', 'envs:random_maze-v0', '1', '10', '5e-05', '10000'),
+    # ('RNN', '1', 'envs:random_maze-v0', '1', '1', '0.0005', '10000'),
+    # ('RNN', '2', 'envs:random_maze-v0', '1', '100', '0.0005', '10000'),
+    # ('RNN', '4', 'envs:random_maze-v0', '1', '100', '5e-05', '10000'),
+    # ('RNN', '8', 'envs:random_maze-v0', '1', '1', '5e-05', '10000'),
     ('FFN', '1', 'CartPole-v1', '1', '100', '0.0005', '10000'),
     ('RNN', '1', 'CartPole-v1', '10', '100', '0.005', '10000'),
     ('RNN', '2', 'CartPole-v1', '10', '1000', '0.005', '10000'),
@@ -146,21 +185,23 @@ BEST_PARAMS = [
     #('RNN', '8', 'CartPole-v1', '10', '1000', '0.005', '10000')] same as above
 
 
+ARG_MAP = {
+    'FFN' : FFN_ARGS,
+    'RNN' : RNN_ARGS,
+    'FFN_MB' : FFN_MB_ARGS,
+    'ZERO_RNN' : ZERO_RNN_ARGS
+}
+
 def experiments(script_args):
-    run_num = 0
-    
+    run_num = 3600
     all_args = []
     bash_file_commands = []
 
     for i in BEST_PARAMS:
         for alg in ['FFN', 'RNN']:
-            for seq_len in [1, 2, 3, 4]:
+            for seq_len in [1, 2, 4, 8]:
                 for seed in SEEDS:
-                    
-                    if alg == 'FFN':
-                        model_args = deepcopy(FFN_ARGS)
-                    else:
-                        model_args = deepcopy(RNN_ARGS) 
+                    model_args = deepcopy(ARG_MAP[alg])
                     
                     general_args = deepcopy(GENERAL_ARGS)
                     general_args['seed'] = int(seed) # int needed to save to json; numpy int32 raises error
